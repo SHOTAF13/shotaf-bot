88a4da5dd247221f1c99832aa8ef85deeb2d203a
SHOTAF13 <shotaf13@gmail.com>
2025-05-30 06:45:30 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// firebase.js
+import admin from 'firebase-admin';
+import { readFileSync } from 'fs';
+
+const serviceAccount = JSON.parse(readFileSync('./google-creds.json.json', 'utf-8'));
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+export const db = admin.firestore();
